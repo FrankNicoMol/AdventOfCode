db9ad5740790ae70b7d9d6336dd7f051feee6a30
FrankNicoMol <frank.mol@rug.nl>
2024-01-09 09:51:28 +0100

    Day 4 updated

--- 2023/day04.py
+++ 2023/day04.py
@@ -1,7 +1,11 @@
 from aoc import read_txt, remove_empty, print_result, results
+import os
+import numpy as np
 
 
 def card_split(card):
+    """Split the string in corresponding values"""
+
     card = card.split(':')[1]
     win = card.split('|')[0].strip().split(' ')
     win = remove_empty(win)
@@ -11,40 +15,58 @@ def card_split(card):
     return win, num
 
 
-def card_points(card):
+def card_points(card, second=False):
+    """Calculate the points of card"""
+
     win, num = card_split(card)
     points = 0
-    first = True
-    for n in num:
-        if n != '' and n in win:
-            if first:
-                points = 1
-                first = False
-            else:
-                points *= 2
+
+    if second:
+        for n in num:
+            if n != '' and n in win:
+                points += 1
+    else:
+        first = True
+        for n in num:
+            if n != '' and n in win:
+                if first:
+                    points = 1
+                    first = False
+                else:
+                    points *= 2
 
     return points
 
 
-def all_points(cards):
-    total_points = 0
-    for card in cards:
-        points = card_points(card)
-        total_points += points
+def all_points(cards, second=False):
+    """Calculate all points"""
+
+    total_points = [card_points(card, second) for card in cards]
+    if not second:
+        total_points = np.sum(total_points)
+
     return total_points
 
 
-def calculate_cards(path, second_part = False):
+def calculate_cards(path, second=False):
+    """Load and calculate"""
+
     cards = read_txt(path)
-    points = all_points(cards)
+    points = all_points(cards, second)
+
+    if second:
+        points = np.array(points)
+
+        mult = np.ones(points.shape)
+        for i in range(len(points)):
+            l = np.min([points[i], len(points) - i])
+            add = np.ones(l) * mult[i]
+            mult[i + 1:i + l + 1] += add
+        points = int(np.sum(mult))
 
     return points
 
-
-
-import os
 
 day = os.path.basename(__file__).split(".")[0]
 
 results(calculate_cards, day)
-
