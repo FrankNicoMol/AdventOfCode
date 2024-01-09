b750f28d2f65213918795db81980150e8b724276
FrankNicoMol <frank.mol@rug.nl>
2024-01-09 15:14:27 +0100

    D08

--- 2023/aoc/io.py
+++ 2023/aoc/io.py
@@ -38,10 +38,12 @@ def results(fun, day):
     """Calculate results for examples, inputs and both parts"""
 
     print_header(day)
-    path = f'input/{day}_'
+    path = f'input/{day}'
 
     files = glob(f'{path}*')
     for second_part in range(2):
         for file in files:
-            value = fun(file, second_part)
-            print_result(day, file.split('_')[1].split('.')[0], second_part, value)
+            if (file.split('_')[0][-2:] == 'p1' and not second_part) or (file.split('_')[0][-2:] == 'p2' and second_part) or (file.split('_')[0][-2:] not in ['p1', 'p2']) :
+                value = fun(file, second_part)
+                print_result(day, file.split('_')[1].split('.')[0], second_part, value)
+
--- 2023/day08.py
+++ 2023/day08.py
@@ -18,12 +18,39 @@ def read_map(file):
 
 def calculate_steps(file, second):
     dirs, maps = read_map(file)
-    dest_map = 'AAA'
-    steps = 0
-    while dest_map != 'ZZZ':
-        dest_map = maps[dest_map][(dirs[steps % len(dirs)] == 'R')]
-        steps += 1
-    return steps
+
+    if not second:
+        dest_map = 'AAA'
+        steps = 0
+        while dest_map != 'ZZZ':
+            dest_map = maps[dest_map][(dirs[steps % len(dirs)] == 'R')]
+            steps += 1
+        return steps
+    else:
+        dest_keys = []
+        for k in maps.keys():
+            if k[-1] == 'A':
+                dest_keys.append(k)
+        dest_keys = dest_keys
+        num_keys = len(dest_keys)
+        finished_keys = 0
+        steps = 0
+        while finished_keys != num_keys:
+            new_dest_keys = []
+            finished_keys = 0
+            for k in dest_keys:
+                key = maps[k][(dirs[steps % len(dirs)] == 'R')]
+                new_dest_keys.append(key)
+
+                if key[-1] == 'Z':
+                    finished_keys += 1
+
+            dest_keys = new_dest_keys
+            steps += 1
+            if steps % 10_000_000 == 0:
+                print(steps)
+
+        return steps
 
 
 day = os.path.basename(__file__).split(".")[0]
