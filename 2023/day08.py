561247b684a2f6c5b7ca6a6a201311ac79b2c0a9
FrankNicoMol <frank.mol@rug.nl>
2024-01-11 09:42:06 +0100

    D08

--- 2023/day08.py
+++ 2023/day08.py
@@ -1,6 +1,13 @@
 from aoc import results, read_txt, remove_empty
 import os
+from math import gcd
 
+def lcm(numbers):
+    """Least common multiple for numbers"""
+    value = 1
+    for n in numbers:
+        value = value*n//gcd(value, n)
+    return value
 
 def read_map(file):
     text = read_txt(file)
@@ -35,22 +42,32 @@ def calculate_steps(file, second):
         num_keys = len(dest_keys)
         finished_keys = 0
         steps = 0
+        reps = []
+        for i in range(len(dest_keys)):
+            reps.append([])
         while finished_keys != num_keys:
             new_dest_keys = []
             finished_keys = 0
-            for k in dest_keys:
+            for i, k in enumerate(dest_keys):
                 key = maps[k][(dirs[steps % len(dirs)] == 'R')]
                 new_dest_keys.append(key)
 
                 if key[-1] == 'Z':
+                    reps[i].append(steps)
                     finished_keys += 1
 
             dest_keys = new_dest_keys
             steps += 1
-            if steps % 10_000_000 == 0:
-                print(steps)
+            if steps == 1_000_000:
+                break
 
-        return steps
+        cycles = []
+        for r in reps:
+            cycles.append(r[1] - r[0])
+
+        value = lcm(cycles)
+
+        return value
 
 
 day = os.path.basename(__file__).split(".")[0]
