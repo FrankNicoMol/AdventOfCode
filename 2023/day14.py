b082f2e41baec000c1de487d636182fb4b2a473a
FrankNicoMol <frank.mol@rug.nl>
2024-01-30 11:03:48 +0100

    Day 14 & 15

--- 2023/day14.py
+++ 2023/day14.py
@@ -23,20 +23,59 @@ def tilt_array(array):
     final_array = np.array([*final_string])
     return final_array
 
-
-def get_value(text, second):
-    board = to_array(text)
-
+def flip_north(board):
     board_tilted = []
     for array in board.T:
         board_tilted.append(tilt_array(array))
     board_final = np.array(board_tilted).T
 
-    h, w = board_final.shape
+    return board_final
+
+def flip_cycle(board):
+
+
+    for i in range(4):
+        board = flip_north(board)
+        board = np.rot90(board, k = 3)
+
+    return board
+
+def cal_load(board):
+    h, w = board.shape
     val_array = np.flip((np.arange(1, h + 1) * np.ones((h, w))).T)
 
-    total = np.sum((board_final == 'O') * val_array)
+    total = np.sum((board == 'O') * val_array)
     total = int(total)
+    return total
+
+
+def get_value(text, second):
+    board = to_array(text)
+
+    if second:
+        reps = 100
+        t = np.zeros(reps)
+        old_sum = np.inf
+        for i in range(reps):
+            board_final = flip_cycle(board)
+            new_sum = np.sum(board_final != board)
+            new_load = cal_load(board_final)
+            t[i] = new_load
+            if new_sum:# and old_sum != new_sum:
+                board = board_final
+                old_sum = new_sum
+            else:
+                break
+
+        #if (1000000000 - i) % 2 == 0:
+            #board_final = flip_cycle(board)
+        total = cal_load(board_final)
+        print(t)
+    else:
+        board_final = flip_north(board)
+        total = cal_load(board_final)
+
+
 
     return total
 
--- 2023/day15.py
+++ 2023/day15.py
@@ -1,11 +1,61 @@
 from aoc import results
 import os
 
+def hash_for_string(string):
+    current_value = 0
+    for c in string:
+        current_value += ord(c)
+        current_value *= 17
+        current_value = current_value % 256
+    return current_value
+
+
+def operation(string):
+
+    if string.find('=') != -1:
+        v = string.find('=')
+        label = string[:v]
+        val = string[v+1:]
+        return label, val, True
+    else:
+        v = string.find('-')
+        label = string[:v]
+        val = ''
+        return label, val, False
+
 
 def get_value(file, second):
-    1 + 1
-    return 5
 
+    strings = file[0].split(',')
+
+    if not second:
+        val = 0
+        for string in strings:
+            val += hash_for_string(string)
+    else:
+        boxes = {}
+
+        for s in strings:
+            label, val, eq = operation(s)
+            hash_label = hash_for_string(label)
+            in_dict = {label: val}
+            if eq:
+                if hash_label in boxes.keys():
+                    boxes[hash_label][label] = val
+                else:
+                    boxes[hash_label] = in_dict
+            else:
+                if hash_label in boxes.keys():
+                    if label in boxes[hash_label].keys():
+                        del boxes[hash_label][label]
+
+        total = 0
+        for key in boxes.keys():
+            for i, label in enumerate(boxes[key].keys()):
+                total += (key + 1) * int(boxes[key][label]) * (i + 1)
+
+        val = total
+    return val
 
 
 day = os.path.basename(__file__).split(".")[0]
