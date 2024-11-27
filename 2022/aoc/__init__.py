11414c5717db3f6f0a50df014018dd145dabbcc3
Frank Mol <frank.mol@rug.nl>
2024-11-27 16:51:26 +0100

    update, transfer to template

--- 2022/aoc/__init__.py
+++ 2022/aoc/__init__.py
@@ -1,17 +1,20 @@
+from pathlib import Path
+
+
 def get_input_path(day, year=2022):
     return f'input/day{day:02d}_input.txt'
 
 
-def get_example_path(day, year=2022):
-    return f'input/day{day:02d}_example.txt'
+def get_lines(day, year=2022):
+    path = get_input_path(day=day)
+
+    with open(path) as file:
+        lines = file.readlines()
+    lines = [line.strip() for line in lines]
+    return lines
 
 
-def get_lines(day, args, year=2022):
-    if len(args) > 1:
-        path = get_example_path(day=day)
-    else:
-        path = get_input_path(day=day)
-
+def get_example_lines(path):
     with open(path) as file:
         lines = file.readlines()
     lines = [line.strip() for line in lines]
@@ -21,3 +24,22 @@ def get_lines(day, args, year=2022):
 def print_header(day, year=2022):
     print(f'Result of Advent of Code, year {year}, day {day}')
     print(f'Find description at https://adventofcode.com/{year}/day/{day}\n')
+
+
+def get_example_paths(day, year=2022):
+    input_path = Path('input')
+    examples_list = [str(p) for p in input_path.glob(f'*{day:02d}_example*')]
+    return examples_list
+
+
+def print_solution(part, solution):
+    print(f'{part:<10}: {solution}')
+
+
+def run_examples(day, f):
+    example_paths = get_example_paths(day)
+    for example_path in example_paths:
+        lines = get_example_lines(example_path)
+        example_solution = f(lines)
+        example_name = example_path.split('.')[0].split('_')[1]
+        print_solution(part=example_name.capitalize(), solution=example_solution)
--- 2022/day01.py
+++ 2022/day01.py
@@ -1,4 +1,4 @@
-from aoc import get_lines, print_header
+from aoc import get_lines, print_header, run_examples, print_solution
 import os
 import sys
 from itertools import groupby
@@ -15,11 +15,29 @@ def get_day_number():
     return day_integer
 
 
+def get_solution(lines, part=1):
+    elves = [sum([int(g) for g in group]) for item, group in groupby(lines, key=bool) if item]
+    elves.sort(reverse=True)
+
+    max_calories = sum(elves[:3]) if part - 1 else elves[0]
+
+    return max_calories
+
+
 if __name__ == "__main__":
     day = get_day_number()
     print_header(day=day)
-    lines = get_lines(day=day, args=sys.argv)
-    elves = [sum([int(g) for g in group]) for item, group in groupby(lines, key=bool) if item]
-    elves.sort(reverse=True)
-    max_calories = sum(elves[:3])
-    print(max_calories)
+
+    # Examples
+    run_examples(day, lambda x: get_solution(x, part=1))
+
+    lines = get_lines(day=day)
+
+    # Part 1
+    solution_p1 = get_solution(lines, part=1)
+    print_solution(part='Part I', solution=solution_p1)
+
+    # Part 2
+    solution_p2 = get_solution(lines, part=2)
+    print_solution(part='Part II', solution=solution_p2)
+
