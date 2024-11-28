77270312cb342f0a990a1d43aebd0a80a1bd52db
Frank Mol <frank.mol@rug.nl>
2024-11-28 10:34:17 +0100

    day3 2022

--- 2022/day03.py
+++ 2022/day03.py
@@ -34,8 +34,38 @@ def solver(day):
     # Part 1
     print_solutions(lines, part=1)
 
+    run_examples(day, lambda x: get_solution(x, part=2))
+
     # Part 2
     print_solutions(lines, part=2)
+
+
+def duplicates(line):
+    line_length = len(line)
+    l1 = line[:line_length // 2]
+    l2 = line[line_length // 2:]
+
+    dup = list(set([c1 for c1 in l1 if c1 in l2]))
+
+    return dup
+
+
+def get_priority(items):
+    priority_val = 0
+    for item in items:
+        if item.isupper():
+            priority_val += ord(item) - 38
+        else:
+            priority_val += ord(item) - 96
+    return priority_val
+
+
+def same_in_set(line_set):
+    l1, l2, l3 = line_set
+
+    dup = list(set([c1 for c1 in l1 if c1 in l2 and c1 in l3]))
+
+    return dup
 
 
 def get_solution(lines, part=1):
@@ -45,11 +75,17 @@ def get_solution(lines, part=1):
 
     # Part I
     if not part - 1:
-        solution = ''
+        solution = 0
+        for line in lines:
+            dup = duplicates(line)
+            solution += get_priority(dup)
 
     # Part II
     else:
-        solution = ''
+        solution = 0
+        for line_set in zip(lines[::3], lines[1::3], lines[2::3]):
+            dup = same_in_set(line_set)
+            solution += get_priority(dup)
 
     return solution
 
