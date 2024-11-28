4e41cc404cc804ff02b0f65ebceff6e4b7d6d116
Frank Mol <frank.mol@rug.nl>
2024-11-28 10:58:38 +0100

    day3 2022

--- 2022/day04.py
+++ 2022/day04.py
@@ -34,8 +34,45 @@ def solver(day):
     # Part 1
     print_solutions(lines, part=1)
 
+    run_examples(day, lambda x: get_solution(x, part=2))
+
     # Part 2
     print_solutions(lines, part=2)
+
+
+def get_vals(ran):
+    ran_begin, ran_end = ran.split('-')
+    ran_begin = int(ran_begin)
+    ran_end = int(ran_end)
+    return ran_begin, ran_end
+
+
+def check_if_in(s_in, s_out):
+    covered = False
+
+    sin_begin, sin_end = get_vals(s_in)
+    sout_begin, sout_end = get_vals(s_out)
+
+    if sin_begin >= sout_begin and sin_end <= sout_end:
+        covered = True
+    if sin_begin <= sout_begin and sin_end >= sout_end:
+        covered = True
+
+    return covered
+
+
+def check_overlap(s_in, s_out):
+    covered = False
+
+    sin_begin, sin_end = get_vals(s_in)
+    sout_begin, sout_end = get_vals(s_out)
+
+    if sin_begin >= sout_begin and sin_begin <= sout_end:
+        covered = True
+    if sin_begin <= sout_begin and sin_end >= sout_begin:
+        covered = True
+
+    return covered
 
 
 def get_solution(lines, part=1):
@@ -45,11 +82,20 @@ def get_solution(lines, part=1):
 
     # Part I
     if not part - 1:
-        solution = ''
+        solution = 0
+        for line in lines:
+            s1, s2 = line.split(',')
+            line_in = check_if_in(s1, s2)
+            solution += line_in
+
 
     # Part II
     else:
-        solution = ''
+        solution = 0
+        for line in lines:
+            s1, s2 = line.split(',')
+            line_in = check_overlap(s1, s2)
+            solution += line_in
 
     return solution
 
