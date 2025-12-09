a70bf3ac02d32011399d82c5ca5004f68b5adde6
FrankNicoMol <frank.n.mol@gmail.com>
2025-12-09 13:41:48 +0100

    2025 day 07 *

---
+++ 2025/day07/day07.py
@@ -0,0 +1,113 @@
+import numpy as np
+import os
+from scipy.signal import convolve2d
+import datetime
+
+
+def get_day_number():
+    """Obtain day number from filename."""
+
+    filename = os.path.basename(__file__)
+    no_extension = filename.split(".")[0]
+    day_number = no_extension[-2:]
+    day_integer = int(day_number)
+
+    return day_integer
+
+
+def print_header(year=2025):
+    """Print header of solutions."""
+
+    day = get_day_number()
+    print(f'\nResult of Advent of Code, year {year}, day {day}')
+    print(f'Find description at https://adventofcode.com/{year}/day/{day}\n')
+
+
+def load_input(input_file):
+    """Read the input file and return a list"""
+
+    with open(f'input/{input_file}', 'r') as file:
+        text = file.read().strip().splitlines()
+
+    return text
+
+
+def run_solvers(part1_input, part2_input, part1_example='example.txt', part2_example='example.txt'):
+    """Run solvers for first and second part, including examples."""
+
+    # Part 1
+    if part1_example:
+        solver(part1_example, part=1)
+
+    if part1_input:
+        solver(part1_input, part=1)
+
+    # Part 2
+    if part2_example:
+        solver(part2_example, part=2)
+
+    if part2_input:
+        solver(part2_input, part=2)
+
+
+def solve_part1(text):
+    """Solve part 1."""
+
+    value = 0
+    mat = np.array([[c for c in line] for line in text])
+    for i in range(len(mat) - 2):
+        block = mat[i:i + 3]
+        sindex = np.where(block[0] == "S")[0]
+        if len(sindex) > 0:
+            mat[i + 1, sindex[0]] = "|"
+
+        split_index = np.where(block[1] == "^")[0]
+        if len(split_index) > 0:
+
+            for ind in split_index:
+                if mat[i, ind] == "|":
+                    value += 1
+                    if ind - 1 > -1:
+                        mat[i + 1, ind - 1] = "|"
+                    if ind + 1 < len(mat):
+                        mat[i + 1, ind + 1] = "|"
+
+        split_index = np.where(block[1] == "|")[0]
+        if len(split_index) > 0:
+            for ind in split_index:
+
+                if mat[i + 2, ind] == ".":
+                    mat[i + 2, ind] = "|"
+
+    return value
+
+
+def solve_part2(text):
+    """Solve part 2."""
+    value = 0
+
+    return value
+
+
+def solver(file_name, part=1):
+    """Solver for problem"""
+
+    text = load_input(file_name)
+    value = 0
+    if part == 1:  # and 'example.txt' in file_name:
+        value = solve_part1(text)
+
+    if part == 2 and 'example.txt' in file_name:
+        value = solve_part2(text)
+
+    print(f'Result for Part {part}')
+    print(f'   Loaded: {file_name}')
+    print(f'   The resulting value is: {value}')
+
+
+if __name__ == '__main__':
+    print_header()
+    run_solvers(part1_example='example.txt',
+                part1_input='input.txt',
+                part2_example='example.txt',
+                part2_input='input.txt')
